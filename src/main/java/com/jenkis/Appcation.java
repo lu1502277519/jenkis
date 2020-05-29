c710be0b5fda640021d9e6354d8ffa366e21ed12
lu1502277519 <1502277519@qq.com>
2020-05-29 14:23:36 +0800

    Initial commit

---
+++ src/main/java/com/jenkis/Appcation.java
@@ -0,0 +1,13 @@
+package com.jenkis;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appcation {
+    public static void main(String[] args) {
+        SpringApplication.run(Appcation.class, args);
+    }
+
+}
